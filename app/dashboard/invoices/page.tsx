a26c52a01c814fd5a9f121cf8a59055fe0bdcfeb
Revolutionary-K <413911629@qq.com>
2024-10-27 23:53:03 +0800

    chapter9

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,12 @@
-export default function Page(){
-    return (
-        <p>Invoices Page</p>
-    )
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+
+export default async function Page() {
+  return (
+    <main>
+      <h1>Invoices Page</h1>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart />
+      </div>
+    </main>
+  );
 }
