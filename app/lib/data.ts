f2ec1de84f1857ccbbbb0e661b191e93cce773cc
Revolutionary-K <413911629@qq.com>
2024-10-28 22:09:38 +0800

    chapter11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -88,6 +88,7 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  console.log('query', query);
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
